ea33a0aabe58e83415357802e245e9ac850724ea
Lewin Villar <lewin.villar@gmail.com>
2021-05-27 05:15:53 +0000

    Improvements on Sales Invoice

--- dgii/dgii/doctype/reporte_607/reporte_607.py
+++ dgii/dgii/doctype/reporte_607/reporte_607.py
@@ -14,18 +14,18 @@ class Reporte607(Document):
 
 @frappe.whitelist()
 def get_file_address(from_date, to_date):
-	result = frappe.db.sql("""SELECT cust.tax_id, sinv.ncf, sinv.posting_date, sinv.total_taxes_and_charges, sinv.base_total 
+	result = frappe.db.sql("""SELECT cust.tax_id, sinv.ncf, sinv.posting_date, sinv.total_taxes_and_charges, sinv.tipo_de_ingreso, sinv.base_total 
 		FROM `tabSales Invoice` AS sinv 
 		JOIN tabCustomer AS cust on sinv.customer = cust.name 
 		WHERE sinv.ncf NOT LIKE '%s' AND cust.tax_id > 0 AND sinv.docstatus = 1 AND sinv.posting_date 
 		BETWEEN '%s' AND '%s' """ % ("SINV-%", from_date, to_date), as_dict=True)
 
 	w = UnicodeWriter()
-	w.writerow(['RNC', 'Tipo de RNC', 'NCF', 'NCF modificado', 'Fecha de impresion', 'ITBIS facturado', 'Monto Total'])
+	w.writerow(['RNC', 'Tipo de RNC', 'NCF', 'NCF modificado', 'Fecha de impresion', 'ITBIS facturado', 'Tipo de Ingreso', 'Monto Total'])
 		
 	for row in result:
 		tipo_rnc = frappe.get_value("Customer", {"tax_id": row.tax_id }, ["tipo_rnc"])
-		w.writerow([row.tax_id, tipo_rnc, row.ncf, "", row.posting_date.strftime("%Y%m%d"), row.total_taxes_and_charges, row.base_total])
+		w.writerow([row.tax_id.replace("-", ""), tipo_rnc, row.ncf, "", row.posting_date.strftime("%Y%m%d"), row.total_taxes_and_charges, row.tipo_de_ingreso, row.base_total])
 
 	frappe.response['result'] = cstr(w.getvalue())
 	frappe.response['type'] = 'csv'
