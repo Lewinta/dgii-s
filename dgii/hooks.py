28e280916628eeea8be92f44f9d42aa74bca3789
frappe <ftoribio@torquezscript.com>
2021-11-19 03:42:18 +0000

    Added fixtures for dgii app in v13

--- dgii/hooks.py
+++ dgii/hooks.py
@@ -16,55 +16,248 @@ app_license = "MIT"
 
 # Fixtures
 # --------
+# # Company
+# # Custom Field
+# "Company-default_payable_account_usd",
+# # Customer
+# # Custom Field
+# "Customer-tipo_rnc",
+# # Property Setter
+# "Customer-tax_id-label",
+# "Customer-quick_entry",
+# "Customer-naming_series-hidden",
+# "Customer-naming_series-reqd",
+
+# # Item Barcode
+# # Property Setter
+# "Item Barcode-barcode-hidden",
+
+# # Item Default
+# # Property Setter
+# "Item Default-default_price_list-in_list_view",
+# "Item Default-default_warehouse-in_list_view",
+# "Item Default-income_account-in_list_view",
+# "Item Default-expense_account-in_list_view",
+# "Item Default-company-in_list_view",
+# # Item
+# # Custom Field
+# "Item-item_type",
+# # Landed Cost Item
+# # Custom Field
+# "Landed Cost Item-weight_per_unit",
+# "Landed Cost Item-weight",
+# # Landed Cost Taxes And Charges
+# # Custom Field
+# "Landed Cost Taxes and Charges-column_break_7",
+# "Landed Cost Taxes and Charges-total",
+# "Landed Cost Taxes and Charges-tax_rate",
+# "Landed Cost Taxes and Charges-create_invoice",
+# "Landed Cost Taxes and Charges-description_sb",
+# "Landed Cost Taxes and Charges-invoice",
+# "Landed Cost Taxes and Charges-totals",
+# "Landed Cost Taxes and Charges-supplier_invoice",
+# "Landed Cost Taxes and Charges-date",
+# "Landed Cost Taxes and Charges-currency",
+# "Landed Cost Taxes and Charges-supplier",
+# "Landed Cost Taxes and Charges-expiry_date",
+# "Landed Cost Taxes and Charges-purchase_taxes_and_charges_template",
+# "Landed Cost Taxes and Charges-cost_center",
+# "Landed Cost Taxes and Charges-tipo_bienes_y_servicios_comprados",
+# "Landed Cost Taxes and Charges-is_petty_cash",
+# "Landed Cost Taxes and Charges-supplier_invoice_no",
+# "Landed Cost Taxes and Charges-tax_amount",
+# # Property Setter
+# "Landed Cost Taxes and Charges-editable_grid",
+# "Landed Cost Taxes and Charges-invoice-no_copy",
+# "Landed Cost Taxes and Charges-supplier_invoice-no_copy",
+# "Landed Cost Taxes and Charges-amount-read_only",
+# "Landed Cost Taxes and Charges-amount-default",
+# "Landed Cost Taxes and Charges-read_only_onload",
+# "Landed Cost Taxes and Charges-transaction_group-width",
+# "Landed Cost Taxes and Charges-description-reqd",
+# "Landed Cost Taxes and Charges-amount-columns",
+# "Landed Cost Taxes and Charges-description-in_list_view",
+# # Purchase Invoice Item
+# # Custom Field
+# "Purchase Invoice Item-item_type",
+
+# # Purchase Invoice
+# # Custom Field
+# "Purchase Invoice-tipo_bienes_y_servicios_comprados",
+# "Purchase Invoice-monto_facturado_bienes",
+# "Purchase Invoice-monto_facturado_servicios",
+# "Purchase Invoice-retention_amount",
+# "Purchase Invoice-include_retention",
+# "Purchase Invoice-isr_rate",
+# "Purchase Invoice-isr_amount",
+# "Purchase Invoice-include_isr",
+# "Purchase Invoice-excise_tax",
+# "Purchase Invoice-other_taxes",
+# "Purchase Invoice-legal_tip",
+# "Purchase Invoice-total_itbis",
+# "Purchase Invoice-supplier_invoice_no",
+# "Purchase Invoice-vencimiento_ncf",
+# "Purchase Invoice-column_break_80",
+# "Purchase Invoice-section_break_88",
+# "Purchase Invoice-retenciones",
+# "Purchase Invoice-retention_type",
+# "Purchase Invoice-retention_rate",
+# # Property Setter
+# "Purchase Invoice-set_posting_time-default",
+# "Purchase Invoice-posting_date-default",
+# "Purchase Invoice-bill_no-in_standard_filter",
+# "Purchase Invoice-cost_center-in_standard_filter",
+# "Purchase Invoice-remarks-reqd",
+# "Purchase Invoice-bill_date-reqd",
+# "Purchase Invoice-payment_schedule-print_hide",
+# "Purchase Invoice-due_date-print_hide",
+# "Purchase Invoice-bill_no-label",
+# "Purchase Invoice-cost_center-reqd",
+# "Purchase Invoice-scan_barcode-hidden",
+# "Purchase Invoice-in_words-print_hide",
+# "Purchase Invoice-in_words-hidden",
+# "Purchase Invoice-rounded_total-print_hide",
+# "Purchase Invoice-rounded_total-hidden",
+# "Purchase Invoice-base_rounded_total-print_hide",
+# "Purchase Invoice-base_rounded_total-hidden",
+# "Purchase Invoice-naming_series-options",
+
+# # Sales Invoice
+# # Custom Field
+# "Sales Invoice-tipo_de_anulacion",
+# "Sales Invoice-tipo_de_factura",
+# "Sales Invoice-return_against_ncf",
+# "Sales Invoice-ncf",
+# "Sales Invoice-tipo_de_ingreso",
+
+# # Sales Taxes and Charges
+# # Custom Field
+# "Sales Taxes and Charges-tax_type",
+
+# # Supplier
+# # Custom Field
+# "Supplier-tipo_rnc",
+# # Property Setter
+# "Supplier-naming_series-hidden",
+# "Supplier-naming_series-reqd",
+# "Supplier-tax_id-in_list_view",
+# "Supplier-pan-hidden",
+# "Supplier-quick_entry",
+# "Supplier-tax_id-in_standard_filter",
 
 fixtures = [
-	{
-		"doctype": "Custom Field",
-		"filters": {
-			"name": (
-				"in", (
-					"Customer-tipo_rnc",
-					"Item-item_type",
-					"Purchase Invoice Item-item_type",
-					"Purchase Invoice-excise_tax",
-					"Purchase Invoice-include_isr",
-					"Purchase Invoice-include_retention",
-					"Purchase Invoice-isr_amount",
-					"Purchase Invoice-isr_rate",
-					"Purchase Invoice-legal_tip",
-					"Purchase Invoice-monto_facturado_bienes",
-					"Purchase Invoice-monto_facturado_servicios",
-					"Purchase Invoice-other_taxes",
-					"Purchase Invoice-retention_amount",
-					"Purchase Invoice-retention_rate",
-					"Purchase Invoice-tax_id",
-					"Purchase Invoice-tipo_bienes_y_servicios_comprados",
-					"Purchase Invoice-total_itbis",
-					"Sales Invoice-ncf",
-					"Sales Invoice-return_against_ncf",
-					"Sales Invoice-tipo_de_anulacion",
-					"Sales Taxes and Charges-tax_type",
-					"Supplier-tipo_rnc",
-				)
-			)
-		}
-	},
-	{
-		"doctype": "Property Setter",
-		"filters": {
-			"name": (
-				"in", (
-					"Sales Invoice-naming_series-options",
-					"Sales Invoice-naming_series-description",
-					"Sales Invoice-naming_series-default",
-					"Sales Invoice-naming_series-print_hide",
-					"Sales Invoice-naming_series-label",
-					"Sales Invoice-naming_series-bold",
-					"Sales Invoice-naming_series-reqd",
-				)
-			)
-		}
-	}	
+    {
+        "doctype": "Custom Field",
+        "filters": {
+            "name": (
+                "in", (
+                    "Company-default_payable_account_usd",
+                    "Customer-tipo_rnc",
+                    "Item-item_type",
+                    "Landed Cost Item-weight_per_unit",
+                    "Landed Cost Item-weight",
+                    "Landed Cost Taxes and Charges-column_break_7",
+                    "Landed Cost Taxes and Charges-total",
+                    "Landed Cost Taxes and Charges-tax_rate",
+                    "Landed Cost Taxes and Charges-create_invoice",
+                    "Landed Cost Taxes and Charges-description_sb",
+                    "Landed Cost Taxes and Charges-invoice",
+                    "Landed Cost Taxes and Charges-totals",
+                    "Landed Cost Taxes and Charges-supplier_invoice",
+                    "Landed Cost Taxes and Charges-date",
+                    "Landed Cost Taxes and Charges-currency",
+                    "Landed Cost Taxes and Charges-supplier",
+                    "Landed Cost Taxes and Charges-expiry_date",
+                    "Landed Cost Taxes and Charges-purchase_taxes_and_charges_template",
+                    "Landed Cost Taxes and Charges-cost_center",
+                    "Landed Cost Taxes and Charges-tipo_bienes_y_servicios_comprados",
+                    "Landed Cost Taxes and Charges-is_petty_cash",
+                    "Landed Cost Taxes and Charges-supplier_invoice_no",
+                    "Landed Cost Taxes and Charges-tax_amount",
+                    "Purchase Invoice Item-item_type",
+                    "Purchase Invoice-tipo_bienes_y_servicios_comprados",
+                    "Purchase Invoice-monto_facturado_bienes",
+                    "Purchase Invoice-monto_facturado_servicios",
+                    "Purchase Invoice-retention_amount",
+                    "Purchase Invoice-include_retention",
+                    "Purchase Invoice-isr_rate",
+                    "Purchase Invoice-isr_amount",
+                    "Purchase Invoice-include_isr",
+                    "Purchase Invoice-excise_tax",
+                    "Purchase Invoice-other_taxes",
+                    "Purchase Invoice-legal_tip",
+                    "Purchase Invoice-total_itbis",
+                    "Purchase Invoice-supplier_invoice_no",
+                    "Purchase Invoice-vencimiento_ncf",
+                    "Purchase Invoice-column_break_80",
+                    "Purchase Invoice-section_break_88",
+                    "Purchase Invoice-retenciones",
+                    "Purchase Invoice-retention_type",
+                    "Purchase Invoice-retention_rate",
+                    "Sales Invoice-tipo_de_anulacion",
+                    "Sales Invoice-tipo_de_factura",
+                    "Sales Invoice-return_against_ncf",
+                    "Sales Invoice-ncf",
+                    "Sales Invoice-tipo_de_ingreso",
+                    "Sales Taxes and Charges-tax_type",
+                    "Supplier-tipo_rnc"
+                )
+            )
+        }
+    },
+    {
+        "doctype": "Property Setter",
+        "filters": {
+            "name": (
+                "in", (
+                    "Customer-tax_id-label",
+                    "Customer-quick_entry",
+                    "Customer-naming_series-hidden",
+                    "Customer-naming_series-reqd",
+                    "Item Barcode-barcode-hidden",
+                    "Item Default-default_price_list-in_list_view",
+                    "Item Default-default_warehouse-in_list_view",
+                    "Item Default-income_account-in_list_view",
+                    "Item Default-expense_account-in_list_view",
+                    "Item Default-company-in_list_view",
+                    "Landed Cost Taxes and Charges-editable_grid",
+                    "Landed Cost Taxes and Charges-invoice-no_copy",
+                    "Landed Cost Taxes and Charges-supplier_invoice-no_copy",
+                    "Landed Cost Taxes and Charges-amount-read_only",
+                    "Landed Cost Taxes and Charges-amount-default",
+                    "Landed Cost Taxes and Charges-read_only_onload",
+                    "Landed Cost Taxes and Charges-transaction_group-width",
+                    "Landed Cost Taxes and Charges-description-reqd",
+                    "Landed Cost Taxes and Charges-amount-columns",
+                    "Landed Cost Taxes and Charges-description-in_list_view",
+                    "Purchase Invoice-set_posting_time-default",
+                    "Purchase Invoice-posting_date-default",
+                    "Purchase Invoice-bill_no-in_standard_filter",
+                    "Purchase Invoice-cost_center-in_standard_filter",
+                    "Purchase Invoice-remarks-reqd",
+                    "Purchase Invoice-bill_date-reqd",
+                    "Purchase Invoice-payment_schedule-print_hide",
+                    "Purchase Invoice-due_date-print_hide",
+                    "Purchase Invoice-bill_no-label",
+                    "Purchase Invoice-cost_center-reqd",
+                    "Purchase Invoice-scan_barcode-hidden",
+                    "Purchase Invoice-in_words-print_hide",
+                    "Purchase Invoice-in_words-hidden",
+                    "Purchase Invoice-rounded_total-print_hide",
+                    "Purchase Invoice-rounded_total-hidden",
+                    "Purchase Invoice-base_rounded_total-print_hide",
+                    "Purchase Invoice-base_rounded_total-hidden",
+                    "Purchase Invoice-naming_series-options",
+                    "Supplier-naming_series-hidden",
+                    "Supplier-naming_series-reqd",
+                    "Supplier-tax_id-in_list_view",
+                    "Supplier-pan-hidden",
+                    "Supplier-quick_entry",
+                    "Supplier-tax_id-in_standard_filter",
+                )
+            )
+        }
+    }
 ]
 
 # Includes in <head>
@@ -96,12 +289,12 @@ after_migrate = "dgii.hook.migrate.after_migrate"
 # Doctype JS
 # ----------
 doctype_js = {
-	"Purchase Invoice": "public/js/purchase_invoice.js",
-	"Customer": "public/js/customer.js",
-	"Supplier": "public/js/supplier.js",
-	"Landed Cost Voucher": "public/js/landed_cost_voucher.js",
-	"Company": "public/js/company.js",
-	"Purchase Order": "public/js/purchase_order.js",
+    "Purchase Invoice": "public/js/purchase_invoice.js",
+    "Customer": "public/js/customer.js",
+    "Supplier": "public/js/supplier.js",
+    "Landed Cost Voucher": "public/js/landed_cost_voucher.js",
+    "Company": "public/js/company.js",
+    "Purchase Order": "public/js/purchase_order.js",
 }
 
 # Generators
@@ -139,17 +332,17 @@ doctype_js = {
 # Hook on document methods and events
 
 doc_events = {
-	# erpnext
-	"Purchase Invoice": {
-		"validate": "dgii.hook.purchase_invoice.validate",
-		"before_submit": "dgii.hook.purchase_invoice.before_submit",
-	},
+    # erpnext
+    "Purchase Invoice": {
+        "validate": "dgii.hook.purchase_invoice.validate",
+        "before_submit": "dgii.hook.purchase_invoice.before_submit",
+    },
     "Sales Invoice": {
         "autoname": "dgii.hook.sales_invoice.autoname",
         "before_insert": "dgii.hook.sales_invoice.before_insert",
         "on_change": "dgii.hook.sales_invoice.on_change",
     },
-	"Landed Cost Voucher": {
+    "Landed Cost Voucher": {
         "on_submit": "dgii.hook.landed_cost_voucher.on_submit",
     },
 }
@@ -186,4 +379,3 @@ doc_events = {
 # override_whitelisted_methods = {
 # 	"frappe.desk.doctype.event.event.get_events": "dgii.event.get_events"
 # }
-	
